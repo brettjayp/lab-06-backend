7835a2f46b873a33a8da377cd7a499b87d2aa363
enoch-strok <enochstrok@outlook.com>
2020-01-21 23:18:08 -0800

    edited server.js file and need to run node server.js, not working on my machine, going to run it on Brett's Machine.

--- server.js
+++ server.js
@@ -3,5 +3,20 @@
 //load Envicronment variables from the .env
 require('dotenv').config();
 
+//Declare Application Dependancies
 const express = require('express');
 const cors = require('cors');
+
+//Application Setup
+const PORT = process.env.PORT;
+const app = express ();
+app.use(cors());
+ 
+//route syntax = app.<operation>('<route>', callback );
+app.get('/', (request, response) => {
+  response.send('Home Page!');
+});
+
+//Ensure the server is listening for request
+//THIS MUST BE AT THE END OF THE FILE!!!
+app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
