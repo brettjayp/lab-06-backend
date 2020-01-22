d89f1772e0e5942f2ef0bd69c8990664d759d17e
Brett Packard <brettjayp@gmail.com>
2020-01-21 23:36:29 -0800

    more server setup

--- server.js
+++ server.js
@@ -17,6 +17,21 @@ app.get('/', (request, response) => {
   response.send('Home Page!');
 });
 
+app.get('/bad', (request, response) => {
+  response.send('OOps, this is THE bad route')
+})
+
+app.get('/home', (request, response) => {
+  response.send('Home Page number 2!');
+})
+
+app.get('/about', aboutUsHandler);
+
+function aboutUsHandler(request, response) {
+  response.status(200).send('About us page');
+}
+
+
 //Ensure the server is listening for request
 //THIS MUST BE AT THE END OF THE FILE!!!
 app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
